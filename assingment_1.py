b24cc4f996a2f22825489e5a26afb9791ae83990
Agnes Schneider <euboia@gmail.com>
2019-04-30 23:13:15 +0200

    Add files via upload

---
+++ assingment_1.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# 1) The following line causes a SyntaxError. Please correct the line so that it's output is 'Hallo Welt!' [1P]
+#print(Hallo Welt!)
+
+# it should be:
+print("Hallo Welt!")
+
+# 2) Calculate the difference between a and b and assign the result to a variable called 'v_1'. 
+#What are the datatypes of a, b and 'v_1'? [2P]
+
+a = 976.543
+b = 345
+
+v_1 = a-b
+print(v_1)
+
+print(type(a))
+print(type(b))
+print(type(v_1))
+
+# 3) Calculate the remainder of the division 100/17 by only using one operator 
+#and save the result in v_2 (look into the Python docs for help) [1P]
+
+v_2 = 100 % 17
+print(v_2)
+
+# 4) The speed of light is about 300'000 km/s. Calculate the wavelength (in nanometers) of a light wave with a 
+#frequency of 5.0E+14 per second. Can we see this light? [4P]
+#    Save the result in v_3.
+
+lsp = 3.0E+5
+f = 5.0E+14
+w = lsp/f
+print(f)
+print(w)
+
+v_3 = lsp/f
+
+#5) Print the following string to the console using the format-function and the variables 'n' and 'pi': 
+#"Pi rounded to the first 10 decimals is: 3.1415926536" [2P]
+n  = 10
+pi = 3.14159265358979323846264338
+
+print("Pi rounded to the first 10 decimals is: " "{0:10.10f}".format(pi))
+
+# ------------------------------------
+# - Don't change stuff below this line
+# ------------------------------------
+
+print(v_1)
+print(v_2)
+print(v_3)
